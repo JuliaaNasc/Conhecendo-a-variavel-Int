7c0f81684c875f890f2ab42315b3bd533a4dd5de
Júlia Nascimento <juliavcostan@gmail.com>
2023-09-12 13:59:26 -0300

    Esse código funciona da seguinte forma: De inicio para da inicio ao aprendizado das variáveis como no exemplo a do tipo (Int). Como no exemplo, é demostrada algumas formas de mostrar ao usuário uma determinada resposta.

---
+++ main.dart
@@ -0,0 +1,25 @@
+void main() {
+  int numeros = 9;
+  print('');
+  // tambem pode ser (numeros += 3);
+  numeros = numeros + 3;
+
+  print(
+      'ESTÁ AQUI O RESULTADO DA EQUAÇÃO MATEMATICA DOS SEUS NÚMEROS:  ${numeros}');
+  print('');
+
+  numeros++;
+
+  print('COM O COMNANDO DE ADIÇÃO, SEU NÚMERO FICA: ${numeros}');
+  print('');
+
+  numeros--;
+  print('COM O COMNANDO DE SUBTRAÇÃO, SEU NÚMERO FICA: ${numeros}');
+  print('');
+
+// + somar
+// - subtrair
+// / dividir
+// * multiplicar
+// % resto
+}
